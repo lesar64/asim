7c8b5a22a0e299d6011274b58487a080a90a00dc
Jan F <54540187+lesar64@users.noreply.github.com>
2022-01-07 02:22:08 +0100

    Basic GPT-3 call functions done.

--- API/tasks.py
+++ API/tasks.py
@@ -1,49 +1,94 @@
 import openai
+import requests
 
+# Config
 
-# Test request
-def classification(query):
-    result = openai.Classification.create(
-        search_model="ada",
-        model="curie",
-        examples=[
-            ["A happy moment", "Positive"],
-            ["I am sad.", "Negative"],
-            ["I am feeling awesome", "Positive"]
-        ],
-        query=query,
-        labels=["Positive", "Negative", "Neutral"],
-    )
+# openai.organization = "org-FDLaKQ4DsHmJstN7LGujAHHq"
+# openai.api_key = "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"  # API_Key Christian
 
-    return result
-
+auth_header = {'Authorization': 'Bearer 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', # API_Key Jan
+               'OpenAI-Organization': 'org-FDLaKQ4DsHmJstN7LGujAHHq'}
 
 # Tasks to fulfill: Summarize, Ask Question, Classify Topic, Create Quote, Explain Code, Create Code,
 # Explain Formula, Code <> Formula (Code to formula; Formula to code)
 
 
-def summarize(query):
-    result = "PLACEHOLDER"
+def gpt_request(engine_id, request_body):
+
+    request = requests.post(f"https://api.openai.com/v1/engines/{engine_id}/completions",
+                            json=request_body,
+                            headers=auth_header)
+
+    result = request.json()['choices'][0]['text'].lstrip('\n')
 
     return result
 
 
-def ask_question(query):
-    result = "PLACEHOLDER"
+def summarize(input_text):
 
-    return result
+    prompt = input_text + '\ntl;dr:\n'
+    # print(prompt)
+    engine_id = 'davinci-instruct-beta-v3'
+
+    request_body = {
+        'prompt': prompt,
+        "max_tokens": 60,
+        "temperature": 0.3,
+    }
+
+    return gpt_request(engine_id, request_body)
 
 
-def classify_topic(query):
-    result = "PLACEHOLDER"
+def ask_question(input_text, question):
 
-    return result
+    prompt = 'Answer a question based on the following text:\n###\n' + input_text + '\n###\n' + question + '\n###\n'
+    # print(prompt)
+    engine_id = 'davinci-instruct-beta-v3'
+
+    request_body = {
+        'prompt': prompt,
+        "max_tokens": 100,
+        "temperature": 0.3,
+        "frequency_penalty": 1,
+        "stop": '###'
+    }
+
+    return gpt_request(engine_id, request_body)
 
 
-def create_quote(query):
-    result = "PLACEHOLDER"
+def classify_topic(input_text):
 
-    return result
+    prompt = 'What is the topic of the following text in one word:\n###\n' + input_text + '\n###\n'
+
+    engine_id = 'davinci-instruct-beta-v3'
+
+    request_body = {
+        'prompt': prompt,
+        "max_tokens": 20,
+        "temperature": 0.3,
+        "frequency_penalty": 1,
+        "stop": '###'
+    }
+
+    return gpt_request(engine_id, request_body)
+
+
+def create_quote(input_text):
+
+    prompt = 'Paraphrase the following sentence:\n' + input_text + '\n'
+    print(prompt)
+
+    engine_id = 'davinci-instruct-beta-v3'
+
+    request_body = {
+        'prompt': prompt,
+        "max_tokens": 60,
+        "temperature": 1,
+        "frequency_penalty": 2,
+        "presence_penalty": 0.5,
+    }
+
+    return gpt_request(engine_id, request_body)
 
 
 def explain_code(query):
