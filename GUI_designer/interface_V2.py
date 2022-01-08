868ff90aff2dd9e023c04c0a9ec87c2e1c30d322
christianschwendner <christian-schwendner@online.de>
2022-01-08 18:48:49 +0100

    Draft design incl. functions finished

--- GUI_designer/interface_V2.py
+++ GUI_designer/interface_V2.py
@@ -10,10 +10,10 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
+
+
 sys.path.append("..")
 from API.tasks import summarize, ask_question, classify_topic, create_quote
-
-
 
 
 class Ui_Dialog(object):
@@ -25,10 +25,14 @@ class Ui_Dialog(object):
         self.TextOutput.setGeometry(QtCore.QRect(590, 200, 550, 650))
         self.TextOutput.setObjectName("TextOutput")
 
-
-        self.TextInput = QtWidgets.QTextEdit(Dialog)
-        self.TextInput.setGeometry(QtCore.QRect(20, 30, 550, 820))
+        self.TextInput = QtWidgets.QTextEdit(Dialog, acceptRichText=False)
+        self.TextInput.setGeometry(QtCore.QRect(20, 30, 550, 741))
         self.TextInput.setObjectName("TextInput")
+        self.TextInput.setTextColor(QtGui.QColor('#000000'))
+
+        self.Question_Input = QtWidgets.QTextEdit(Dialog)
+        self.Question_Input.setGeometry(QtCore.QRect(20, 810, 550, 41))
+        self.Question_Input.setObjectName("Question_Input")
 
         def update_output(function_type):
             Input_text = self.TextInput.toPlainText()
@@ -39,11 +43,13 @@ class Ui_Dialog(object):
                 result = create_quote(Input_text)
             elif function_type == 'classify_topic':
                 result = classify_topic(Input_text)
+            elif function_type == 'ask_question':
+                Question_text = self.Question_Input.toPlainText()
+                result = ask_question(Input_text, Question_text)
 
             self.TextOutput.setText(result)
 
             return
-
 
         self.label = QtWidgets.QLabel(Dialog)
         self.label.setGeometry(QtCore.QRect(20, 8, 61, 21))
@@ -52,9 +58,8 @@ class Ui_Dialog(object):
         font.setBold(True)
         font.setWeight(75)
         self.label.setFont(font)
-        self.label.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
         self.label.setObjectName("label")
-
 
         self.label_2 = QtWidgets.QLabel(Dialog)
         self.label_2.setGeometry(QtCore.QRect(590, 170, 81, 31))
@@ -63,8 +68,18 @@ class Ui_Dialog(object):
         font.setBold(True)
         font.setWeight(75)
         self.label_2.setFont(font)
-        self.label_2.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.label_2.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter)
         self.label_2.setObjectName("label_2")
+
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(20, 780, 91, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_3.setFont(font)
+        self.label_3.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
+        self.label_3.setObjectName("label_3")
 
         # Question
         self.ButtonQuestion = QtWidgets.QPushButton(Dialog)
@@ -75,9 +90,7 @@ class Ui_Dialog(object):
         font.setWeight(50)
         self.ButtonQuestion.setFont(font)
         self.ButtonQuestion.setObjectName("ButtonQuestion")
-        question=input()
-        # self.ButtonQuestion.clicked.connect(ask_question(input_phrase, question))
-        self.ButtonQuestion.clicked.connect(lambda: print(self.TextInput.toPlainText()))
+        self.ButtonQuestion.clicked.connect(lambda: update_output('ask_question'))
 
         # Quote
         self.ButtonQuote = QtWidgets.QPushButton(Dialog)
@@ -101,7 +114,6 @@ class Ui_Dialog(object):
         self.ButtonSummary.setObjectName("ButtonSummary")
         self.ButtonSummary.clicked.connect(lambda: update_output("summarize"))
 
-
         # Topic
         self.ButtonTopic = QtWidgets.QPushButton(Dialog)
         self.ButtonTopic.setGeometry(QtCore.QRect(870, 30, 270, 60))
@@ -113,24 +125,24 @@ class Ui_Dialog(object):
         self.ButtonTopic.setObjectName("ButtonTopic")
         self.ButtonTopic.clicked.connect(lambda: update_output('classify_topic'))
 
-
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        Dialog.setWindowTitle(_translate("Dialog", "GPT-3 supported paper analysis"))
         self.label.setText(_translate("Dialog", "Input"))
         self.label_2.setText(_translate("Dialog", "Output"))
+        self.label_3.setText(_translate("Dialog", "Question"))
         self.ButtonQuestion.setText(_translate("Dialog", "Question"))
         self.ButtonQuote.setText(_translate("Dialog", "Quote"))
         self.ButtonSummary.setText(_translate("Dialog", "Summary"))
         self.ButtonTopic.setText(_translate("Dialog", "Topic"))
 
 
-
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     Dialog = QtWidgets.QDialog()
     ui = Ui_Dialog()
